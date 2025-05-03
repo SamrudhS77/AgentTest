77b26fce2fd1939ca84c7f84cc3f3b857acfb353
Samrudh S <sshe0094@student.monash.edu>
2025-05-03 18:50:28 +1000

    Fetching share prices with summaries

--- smolagents/main.py
+++ smolagents/main.py
@@ -19,18 +19,32 @@ from smolagents import (
 
 #model1 =  HfApiModel(model_id="HuggingFaceH4/zephyr-7b-beta")
 
-model = LiteLLMModel(
-    model_id="meta-llama/Meta-Llama-3-8B-Instruct",
-    api_base="https://api.groq.com/openai/v1",  # Required for Groq
-    api_key=os.getenv("GROQ_API_KEY")           # Picks token from .env
-)
+# model = LiteLLMModel(
+#     model_id="meta-llama/Meta-Llama-3-8B-Instruct",
+#     api_base="https://api.groq.com/openai/v1",  # Required for Groq
+#     api_key=os.getenv("GROQ_API_KEY")           # Picks token from .env
+# )
+
+# Using Hugging Face model
+# model2 = HfApiModel(
+#     model_id="HuggingFaceH4/zephyr-7b-beta"  # Open-access model
+# )
+
+# model3 = HfApiModel(model_id="mistralai/Mistral-7B-Instruct-v0.1")
+
+hfModel = HfApiModel(token = os.getenv("HUGGINGFACEHUB_API_TOKEN"))
 
 agent = ToolCallingAgent(
     tools =[DuckDuckGoSearchTool(), VisitWebpageTool()],
-    model = model1,
+    model = hfModel,
     add_base_tools = True
 )
 
-answer = agent.run("fetch the share price of google from 2020 to 2024, and create a line graph from it?")
+prompt = """
+        Fetch the share price of google from 2020 to 2024, and create a line graph from it?
+        """
+# answer = agent.run(prompt)
 
-print(answer)
+agent.run(prompt)
+
+# print(answer)
