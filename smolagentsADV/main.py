2f3e8f9933ffccd1ec12a0033852e4784297d6d7
Samrudh S <sshe0094@student.monash.edu>
2025-05-07 20:12:50 +1000

    main.py updated with prompt

--- smolagentsADV/main.py
+++ smolagentsADV/main.py
@@ -16,3 +16,23 @@ model = LiteLLMModel(
 
 manager_agent = ManagerAgent()
 
+agent = ToolCallingAgent(
+    tools=[
+        PythonExecutionTool(),
+        FileLoaderTool()
+    ],
+    model = model,
+    manager_agent = manager_agent,
+    add_base_tools = True
+)
+
+prompt = """
+You are a data analyst. The dataset is located at './data/sample.csv'.
+
+1. Load the dataset using 'load_csv'.
+2. Use data exploration agent to summarize and inspect it.
+3. Use data cleaning agent to clean missing data, fix types, and remove duplicates.
+4. Then, use 'python_executor' to generate histograms for numeric columns.
+"""
+
+agent.run(prompt)
