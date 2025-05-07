5cfad90fd775f7fe97d3c43d5ec313d54daca65e
Samrudh S <sshe0094@student.monash.edu>
2025-05-07 20:03:23 +1000

    main.py updated

--- smolagentsADV/main.py
+++ smolagentsADV/main.py
@@ -4,3 +4,15 @@ import os
 from smolagents import ToolCallingAgent, LiteLLMModel
 from tools.python_exec_tool import PythonExecutionTool
 from tools.file_loader_tool import FileLoaderTool
+from agents.manager_agent import ManagerAgent
+
+load_dotenv()
+
+model = LiteLLMModel(
+    model_id= "meta-llama/llama-4-scout-17b-16e-instruct",
+    api_base= "https://api.groq.com/openai/v1/models",
+    api_key = os.getenv("GROQ_API_KEY")
+)
+
+manager_agent = ManagerAgent()
+
